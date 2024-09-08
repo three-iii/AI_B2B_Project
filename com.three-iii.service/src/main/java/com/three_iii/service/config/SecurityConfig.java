cd2dde027ede82ec3c4e1f69ac44375fd1be79fa
linavell <linavell@naver.com>
2024-09-08 16:46:56 +0900

    feat(#1): Spring Security 기본 보안 정책 설정

---
+++ com.three-iii.service/src/main/java/com/three_iii/service/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.three_iii.service.config;
+
+import static org.springframework.http.HttpMethod.POST;
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+            .csrf(AbstractHttpConfigurer::disable)
+            .formLogin(AbstractHttpConfigurer::disable)
+            .httpBasic(AbstractHttpConfigurer::disable)
+            .headers(headers -> headers.frameOptions(FrameOptionsConfig::disable))
+            .sessionManagement(session -> session
+                .sessionCreationPolicy(STATELESS))
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers(POST, "/auth/**").permitAll()
+                .anyRequest().authenticated()
+            )
+            .build();
+    }
+
+}
