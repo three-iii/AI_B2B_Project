cbcdd802e8bf981d672594cfab679478dcb963ad
jiyeonahn <ajy7424@naver.com>
2024-09-09 14:31:53 +0900

    feat(#15): product 도메인 생성

---
+++ com.three-iii.service/src/main/java/com/three_iii/service/domain/Product.java
@@ -0,0 +1,50 @@
+package com.three_iii.service.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "p_product")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn
+    private Company company;
+
+    //TODO 안지연
+    // hub 매핑
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn
+//    private Hub hub;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private int quantity;
+
+    @Column(nullable = false)
+    @Builder.Default
+    private Boolean is_delete = false;
+
+}
