121ba218bff5db73cfae6007e06585b42cc22f79
jiyeonahn <ajy7424@naver.com>
2024-09-09 14:22:30 +0900

    feat(#10): company 이름 중복 검사

--- com.three-iii.service/src/main/java/com/three_iii/service/application/CompanyService.java
+++ com.three-iii.service/src/main/java/com/three_iii/service/application/CompanyService.java
@@ -1,9 +1,12 @@
 package com.three_iii.service.application;
 
+import static com.three_iii.service.exception.ErrorCode.DUPLICATED_NAME;
+
 import com.three_iii.service.application.dto.CompanyCreateRequest;
 import com.three_iii.service.application.dto.CompanyCreateResponse;
 import com.three_iii.service.domain.Company;
 import com.three_iii.service.domain.repository.CompanyRepository;
+import com.three_iii.service.exception.ApplicationException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,6 +19,10 @@ public class CompanyService {
 
     @Transactional
     public CompanyCreateResponse createCompany(CompanyCreateRequest requestDto) {
+        // 업체명 중복 검사
+        if (companyRepository.findByName(requestDto.getName()).isPresent()) {
+            throw new ApplicationException(DUPLICATED_NAME);
+        }
         Company company = CompanyCreateRequest.toEntity(requestDto);
         return CompanyCreateResponse.fromEntity(companyRepository.save(company));
     }
--- com.three-iii.service/src/main/java/com/three_iii/service/exception/ErrorCode.java
+++ com.three-iii.service/src/main/java/com/three_iii/service/exception/ErrorCode.java
@@ -7,8 +7,9 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 @Getter
 public enum ErrorCode {
-    ACCESS_DENIED(HttpStatus.FORBIDDEN, "접근 권한이 없습니다.");
+    ACCESS_DENIED(HttpStatus.FORBIDDEN, "접근 권한이 없습니다."),
 
+    DUPLICATED_NAME(HttpStatus.BAD_REQUEST, "중복된 이름입니다.");
     private HttpStatus status;
     private String message;
 }
