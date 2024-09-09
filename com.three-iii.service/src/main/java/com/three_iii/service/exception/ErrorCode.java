3146f0d74dcd05a1b298becbb39c3d3277423c52
linavell <linavell@naver.com>
2024-09-09 14:20:33 +0900

    feat(#1): 회원가입 API 구현
    - 필요한 도메인 메서드 추가
    - 필요한 Dto, Service, Controller 구현
    - 비밀번호 암호화를 위한 Config 생성
    - AuthService 에 필요한 ErrorCode 추가

--- com.three-iii.service/src/main/java/com/three_iii/service/exception/ErrorCode.java
+++ com.three-iii.service/src/main/java/com/three_iii/service/exception/ErrorCode.java
@@ -7,7 +7,9 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 @Getter
 public enum ErrorCode {
-    ACCESS_DENIED(HttpStatus.FORBIDDEN, "접근 권한이 없습니다.");
+    ACCESS_DENIED(HttpStatus.FORBIDDEN, "접근 권한이 없습니다."),
+    DUPLICATE_USERNAME(HttpStatus.BAD_REQUEST, "중복된 사용자 입니다."),
+    DUPLICATE_SLACK_ID(HttpStatus.BAD_REQUEST, "중복된 Slack Id 입니다.");
 
     private HttpStatus status;
     private String message;
